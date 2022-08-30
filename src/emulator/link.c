c1fa5b515dba0b776e498e7ca2fa1efb668c3dd9
mpostaire <maxime_postaire@hotmail.fr>
2022-08-30 23:20:48 +0200

    fix link freq

--- src/emulator/link.c
+++ src/emulator/link.c
@@ -4,17 +4,16 @@
 #include "cpu.h"
 #include "mmu.h"
 
-// TODO this is still buggy: tetris works fine, dr mario has glitched game over screen (unrelated?) and pokemon red has glitched fight and trade
-// TODO fix problems when a emulator is paused during a connection
-// TODO handle disconnection
+// TODO this is still buggy: tetris works fine, dr mario has glitched game over screen (unrelated?)
+//      and pokemon red has glitched fight and trade
 
 void link_set_clock(emulator_t *emu) {
     if (emu->mode == CGB) {
         // TODO handle double speed (also call this function where the double speed enable/disable is happening)
         if (CHECK_BIT(emu->mmu->mem[SC], 1))
-            emu->link->clock_cycles = GB_CPU_FREQ / 8192;
-        else
             emu->link->clock_cycles = GB_CPU_FREQ / 262144;
+        else
+            emu->link->clock_cycles = GB_CPU_FREQ / 8192;
     } else {
         emu->link->clock_cycles = GB_CPU_FREQ / 8192;
     }
@@ -31,22 +30,6 @@ void link_quit(emulator_t *emu) {
     free(emu->link);
 }
 
-/**
- * This implementation may cause bugs if the 2 emulators wants to communicate using the internal clock
- * at the same time or during the cycles needed to shift the 8 bits of SB. This should be a rare case
- * so it's not really a problem.
- * 
- * Cause of glitches because the 2 emulators are not completely synchronized?
- * Or maybe messages aren't sent/received due to errors? <-- test this first
- * 
- * I'll have to implement a cpu sync by forcing each iteration to take 4 cycles
- * (passing memory timings of blargg's tests) and send the current cycle to the serial link for comparison to do the interrupt at the same
- * time (same cycle)
- * maybe with 512 as limit value, problems are due to the cpu not using the SB byte and being interrupted before it has the time... but unlikely
- * as the interrupts disable other interrupts as they are processed
- * 
- * If emulator on 1 computer faster than the other, may cause problems --> cpu cycle sync important?
- */
 void link_step(emulator_t *emu, int cycles) {
     link_t *link = emu->link;
     mmu_t *mmu = emu->mmu;
