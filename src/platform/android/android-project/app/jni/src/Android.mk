d21c1084dce4e19b49bda5eeb7aa63e475615845
mpostaire <maxime_postaire@hotmail.fr>
2024-01-27 12:06:18 +0100

    change build requirements

--- src/platform/android/android-project/app/jni/src/Android.mk
+++ src/platform/android/android-project/app/jni/src/Android.mk
@@ -16,7 +16,7 @@ LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/$(SRC_PATH)/core/*.c) \
 				   $(wildcard $(LOCAL_PATH)/$(SRC_PATH)/platform/common/*.c) \
 				   $(wildcard $(LOCAL_PATH)/$(SRC_PATH)/platform/android/*.c)
 
-LOCAL_CFLAGS := -std=gnu11 -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wno-bad-function-cast -O3 -D__HAVE_ZLIB__
+LOCAL_CFLAGS := -std=gnu11 -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wno-bad-function-cast -O3
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
