a39fae280e02c854a7bf0271fcbfc727b53bc3f9
mpostaire <maxime_postaire@hotmail.fr>
2025-03-15 22:30:55 +0100

    wip

--- src/core/gba/cpu.h
+++ src/core/gba/cpu.h
@@ -3,12 +3,17 @@
 #include "types.h"
 #include "../utils.h"
 
-#define CPU_FREQ 0x1000000 // 16.8 Mhz
+#define GBA_CPU_FREQ 0x1000000 // 16.8 Mhz
 
 typedef struct {
     uint32_t regs[16];
+
+    uint32_t banked_regs_8_12[2][5];
+    uint32_t banked_regs_13_14[6][2];
+
     uint32_t cpsr; // current program status register
-    uint32_t spsr; // saved program status register
+    uint32_t spsr[6];
+
     uint32_t pipeline[2]; // array of instructions (because it is a 3 stage pipeline, we just need to remember 2 instructions)
 } gba_cpu_t;
 
