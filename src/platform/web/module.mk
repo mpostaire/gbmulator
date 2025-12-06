91feb53fa25ff273a0c5556bd56657f8e5debdc7
mpostaire <maxime_postaire@hotmail.fr>
2025-12-07 00:43:18 +0100

    web: fix compile error

--- src/platform/web/module.mk
+++ src/platform/web/module.mk
@@ -4,14 +4,14 @@ SRC += $(wildcard $(SDIR)/platform/$(PLATFORM)/*.c)
 OBJ := $(SRC:$(SDIR)/%.c=$(ODIR)/%.o)
 BIN := $(ODIR)/index.html
 
-CFLAGS += -Wno-unused-command-line-argument
+CFLAGS += -Wno-unused-command-line-argument -sUSE_ZLIB=1
 LDLIBS += -lopenal -lidbfs.js -lz
 
 CC = emcc
 
 $(BIN): $(SDIR)/platform/$(PLATFORM)/template.html $(OBJ) $(UI_DEPS)
 	$(CC) -o $@ $(OBJ) $(CFLAGS) $(LDLIBS) \
-		-sINITIAL_MEMORY=128MB -sUSE_WEBGL2=1 -sUSE_ZLIB=1 -sWASM=1 \
+		-sINITIAL_MEMORY=128MB -sUSE_WEBGL2=1 -sWASM=1 \
 		-sEXPORTED_FUNCTIONS="['_main', '_malloc', '_free']" \
 		-sEXPORTED_RUNTIME_METHODS="['ccall', 'HEAPU8']" \
 		-sASYNCIFY --shell-file $<
