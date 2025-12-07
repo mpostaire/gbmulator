bb41222a0c44cebb01a2decd58e086389bdb4348
mpostaire <maxime_postaire@hotmail.fr>
2025-12-07 01:01:50 +0100

    desktop: fix makefile install target

--- src/platform/desktop/module.mk
+++ src/platform/desktop/module.mk
@@ -8,17 +8,12 @@ BIN := $(ODIR)/$(BINNAME)
 CFLAGS += $(shell pkg-config --cflags gtk4 libadwaita-1 zlib manette-0.2 opengl openal gstreamer-1.0) -fanalyzer
 LDLIBS += $(shell pkg-config --libs gtk4 libadwaita-1 zlib manette-0.2 opengl openal gstreamer-1.0)
 
-$(BIN): $(OBJ)
-	$(CC) -o $(BIN) $^ $(CFLAGS) $(LDLIBS)
-
 $(UI_RESOURCES).c: $(SDIR)/platform/$(PLATFORM)/ui/$(BINNAME).gresource.xml $(wildcard $(SDIR)/platform/$(PLATFORM)/ui/*.ui)
 	glib-compile-resources $< --target=$@ --generate-source --sourcedir=$(SDIR)/platform/$(PLATFORM)
 
 ICONDIR  := $(ODIR)/icons
 
-ODIR_STRUCTURE += $(ICONDIR)
-
-ICONS= \
+ICONS := \
 	$(ICONDIR)/192x192/$(BINNAME).png \
 	$(ICONDIR)/144x144/$(BINNAME).png \
 	$(ICONDIR)/128x128/$(BINNAME).png \
@@ -31,13 +26,16 @@ ICONS= \
 
 $(ICONS): images/icons/$(BINNAME).svg
 	mkdir -p $(ICONDIR)/$(patsubst $(ICONDIR)/%/$(BINNAME).png,%,$@)
-	magick $^ -background none -density 1200 -resize $(patsubst $(ICONDIR)/%/$(BINNAME).png,%,$@) $(ICONDIR)/$(patsubst $(ICONDIR)/%/$(BINNAME).png,%,$@)/$(BINNAME).png
+	magick -background none -density 1200 -filter Lanczos $^ -resize $(patsubst $(ICONDIR)/%/$(BINNAME).png,%,$@) $(ICONDIR)/$(patsubst $(ICONDIR)/%/$(BINNAME).png,%,$@)/$(BINNAME).png
+
+$(BIN): $(ICONS) $(OBJ)
+	$(CC) -o $(BIN) $(OBJ) $(CFLAGS) $(LDLIBS)
 
 install: $(ODIR_STRUCTURE) $(ICONS) $(BIN)
 	install -m 0755 $(BIN) /usr/bin
 	install -m 0644 $(SDIR)/platform/desktop/$(BINNAME).xml /usr/share/mime/application
 	install -m 0644 $(SDIR)/platform/desktop/$(BINNAME).desktop /usr/share/applications
-	install -m 0644 $(ICONDIR)/$(BINNAME).svg /usr/share/icons/hicolor/scalable/apps
+	install -m 0644 images/icons/$(BINNAME).svg /usr/share/icons/hicolor/scalable/apps
 	install -m 0644 $(ICONDIR)/192x192/$(BINNAME).png /usr/share/icons/hicolor/192x192/apps
 	install -m 0644 $(ICONDIR)/128x128/$(BINNAME).png /usr/share/icons/hicolor/128x128/apps
 	install -m 0644 $(ICONDIR)/96x96/$(BINNAME).png /usr/share/icons/hicolor/96x96/apps
